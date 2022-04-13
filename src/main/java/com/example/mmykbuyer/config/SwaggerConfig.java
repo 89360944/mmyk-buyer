bc0a9e4ac76f41dca9052a3e36a47093fc25ec36
89360944@qq.com <89360944@qq.com>
2022-04-13 16:33:25 +0800

    test

--- src/main/java/com/example/mmykbuyer/config/SwaggerConfig.java
+++ src/main/java/com/example/mmykbuyer/config/SwaggerConfig.java
@@ -4,30 +4,29 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public GroupedOpenApi groupedApi() {
-        return GroupedOpenApi.builder()
-                .group("my-api")
-                .pathsToMatch("/api/**")
-                .build();
-    }
+//    @Bean
+//    public GroupedOpenApi groupedApi() {
+//        return GroupedOpenApi.builder()
+//                .group("my-api")
+//                .pathsToMatch("/api/**")
+//                .build();
+//    }
 
     @Bean
     public OpenAPI api() {
-        return new OpenAPI()
-                .info(new Info().title("My API")
-                        .description("My sample application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("https://blog.csdn.net/zhoudingding")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("My Blog")
-                        .url("https://blog.csdn.net/zhoudingding"));
+        List<Server> servers = new ArrayList<>();
+        servers.add(new Server().url("182.254.192.61"));
+        return new OpenAPI().servers(servers);
     }
 
 }
