081b95530bef1999f0bf8db9d5f2bcb62a4b1f25
89360944@qq.com <89360944@qq.com>
2022-04-20 17:18:07 +0800

    test

--- src/main/java/com/example/mmykbuyer/controller/BuyerController.java
+++ src/main/java/com/example/mmykbuyer/controller/BuyerController.java
@@ -4,64 +4,66 @@ import com.example.mmykbuyer.entity.Buyer;
 import com.example.mmykbuyer.param.*;
 import com.example.mmykbuyer.service.BuyerService;
 import com.example.mmykbuyer.vo.ResponseVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
-@Tag(name = "站点--Buyer")
+@Api(tags = "买家")
 @RestController
+@RequestMapping("/buyer")
 public class BuyerController {
 
     @Autowired
     private BuyerService buyerService;
 
-    @GetMapping("/test")
-    public String test(HttpServletRequest request){
-        return "t hello world, uri:" + request.getRequestURI();
+    @ApiOperation("测试")
+    @GetMapping(value = "/test")
+    public Map<String, String> prod(String storeId, String token) {
+        System.out.println(storeId + ":" + token);
+        Map<String, String> map = new HashMap<>();
+        map.put("storeId", storeId);
+        map.put("token", token);
+        return map;
     }
 
-    @GetMapping("/buyer-service/test")
-    public String test2(HttpServletRequest request){
-        return "uto hello world, uri:" + request.getRequestURI();
-    }
-
-    @Operation(summary = "创建站点")
+    @ApiOperation("创建站点")
     @PostMapping(value = "/createBuyer")
     public ResponseVO prod(@Validated @RequestBody CreateBuyerParam param) {
         buyerService.createBuyer(param);
         return ResponseVO.ok();
     }
 
-    @Operation(summary = "更新站点")
+    @ApiOperation("更新站点")
     @PostMapping(value = "/updateBuyer")
     public ResponseVO prod(@Validated @RequestBody UpdateBuyerParam param) {
         buyerService.updateBuyer(param);
         return ResponseVO.ok();
     }
 
-    @Operation(summary = "删除站点")
+    @ApiOperation("删除站点")
     @PostMapping(value = "/deleteBuyer")
     public ResponseVO prod(@Validated @RequestBody DeleteBuyerParam param) {
         buyerService.deleteBuyer(param);
         return ResponseVO.ok();
     }
 
-    @Operation(summary = "获取站点详情")
+    @ApiOperation("获取站点详情")
     @PostMapping(value = "/getBuyer")
     public ResponseVO<Buyer> prod(@Validated @RequestBody GetBuyerParam param) {
         Buyer buyer = buyerService.getBuyer(param);
         return ResponseVO.ok(buyer);
     }
 
-    @Operation(summary = "获取站点列表")
+    @ApiOperation("获取站点列表")
     @PostMapping(value = "/listBuyer")
     public ResponseVO<Page<Buyer>> prod(@Validated @RequestBody ListBuyerParam param) {
         Page<Buyer> page = buyerService.listBuyer(param);
